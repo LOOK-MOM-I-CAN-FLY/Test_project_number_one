ce8089d34ada8cca54d1459fae70989868ad99be
Sindi_Hall <146654192+Sindihall@users.noreply.github.com>
2024-08-27 14:47:44 +0300

    Update endpoint.go

--- middleware/internal/app/endpoint/endpoint.go
+++ middleware/internal/app/endpoint/endpoint.go
@@ -1,1 +1,34 @@
+package endpoint
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Service interface {
+	DaysLeft() int64
+}
+type Endpoint struct {
+	s Service
+}
+
+func New(s Service) *Endpoint {
+	return &Endpoint{
+		s: s,
+	}
+}
+
+func (e *Endpoint) Status(ctx echo.Context) error {
+
+	days := e.s.DaysLeft()
+	server := fmt.Sprintf("Days left: %d", days)
+	err := ctx.String(http.StatusOK, server)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
