87b5b760730320ebdf9858253d051ae5e78463e6
Sindi_Hall <146654192+Sindihall@users.noreply.github.com>
2024-08-27 14:46:31 +0300

    Update main.go

--- middleware/cmd/umbrella-test-task/main.go
+++ middleware/cmd/umbrella-test-task/main.go
@@ -1,1 +1,18 @@
+package main
 
+import (
+	"log"
+	"middleware/internal/pkg/app"
+)
+
+func main() {
+	app, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = app.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
