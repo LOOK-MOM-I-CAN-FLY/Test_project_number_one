40dd883bc381787c15ca4208e462ea0e388c5f2e
Sindi_Hall <146654192+Sindihall@users.noreply.github.com>
2024-08-29 15:37:10 +0300

    Update mw.go

--- middleware/internal/app/MW/mw.go
+++ middleware/internal/app/MW/mw.go
@@ -1,1 +1,28 @@
+package mw
+
+import (
+	"log"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+const roleAdmin = "admin"
+
+func RoleCheck(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+
+		val := ctx.Request().Header.Get("User-role")
+
+		if strings.EqualFold(val, roleAdmin) {
+			log.Println("Red button user detected")
+		}
+
+		err := next(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
 
